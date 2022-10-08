f3ca41d5d5dc0e8cd0c8776bc4d3e34ac770769e
Sergey <serega110193@mail.ru>
2022-10-08 19:31:42 +0900

    добавление 10 задачи

---
+++ zadacha10/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 10: Напишите метод, который принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+void Main()
+{
+  int secondnumber;
+    Console.Write("Введите 3-х значное число:");
+    int number = Convert.ToInt32(Console.ReadLine());
+
+  if (number < 1000 && number != 000)
+  {
+   secondnumber = ((number % 100)- (number % 10)) / 10;
+   Console.Write("вторая цифра трёхзначного числа = ");
+   Console.WriteLine(secondnumber);
+  }
+  else
+  {
+    Console.Write("неправильно ввели число");
+  }
+}
+
+Main();
