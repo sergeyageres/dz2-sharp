ee040562dcb5fd79615937613cab773741ca1873
Sergey <serega110193@mail.ru>
2022-10-08 20:59:34 +0900

    добавление 13 задачи

---
+++ zadacha13/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 13: Напишите метод, который выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+void Main()
+{
+    int secondnumber;
+    Console.Write("Введите число:");
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    if (number > 99)
+    {
+        secondnumber = (number % 1000) / 100;
+        Console.Write("Третья цифра трёхзначного числа = ");
+        Console.WriteLine(secondnumber);
+    }
+
+    else
+    {
+        Console.Write("Третьей цифры нет");
+    }
+}
+
+Main();
